042f80148ef7e7c725f50ec65c7d15de2c5b4539
ymgyt <yamaguchi7073xtt@gmail.com>
2020-02-06 20:06:06 +0900

    release v0.3.0

---
+++ Formula/mysqlpinger.rb
@@ -0,0 +1,11 @@
+class MySQLPinger < Formula
+  desc "cli mysql utility written in Rust"
+  homepage "https://github.com/ymgyt/mysqlpinger"
+  url "https://github.com/ymgyt/mysqlpinger/releases/download/v0.3.0/mysqlpinger-0.3.0-x86_64-apple-darwin.tar.gz"
+  sha256 "f253213e27eaec55a9c58029de92c84c4bd11f311f77d273e15053a21ba5684c"
+  version "0.3.0"
+
+  def install
+    bin.install "mysqlpinger"
+  end
+end
